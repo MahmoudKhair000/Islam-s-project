055190881fef419d5d0b4db2c6e2d81fb9ccc1f8
MahmoudKh <mahmoudkhair000@outlook.com>
2025-06-10 17:34:07 +0300

    navbar missing off-canvas

--- frontend/customer-interface/src/app/app.routes.ts
+++ frontend/customer-interface/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { UserSignComponent } from '../components/user-sign/user-sign.component';
+import { HomepageComponent } from '../components/homepage/homepage.component';
+import { AboutComponent } from '../components/about/about.component';
+import { ProfileComponent } from '../components/profile/profile.component';
+import { ExploreComponent } from '../components/explore/explore.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        title: "Home Page",
+        path: "",
+        component: HomepageComponent
+    },
+    {
+        title: "Explore",
+        path: "explore",
+        component: ExploreComponent,
+    },
+    {
+        title: "User - Sign",
+        path: "user/sign",
+        component: UserSignComponent,
+    },
+    {
+        title: "Profile",
+        path: "user/profile",
+        component: ProfileComponent
+    },
+    {
+        title: "About Us",
+        path: "about",
+        component: AboutComponent
+    },
+];
---
+++ frontend/customer-interface/src/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [FormsModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
