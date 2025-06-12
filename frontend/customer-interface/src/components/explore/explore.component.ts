b1f71dfe6780fbe821533016434bafe99d0e3393
MahmoudKh <mahmoudkhair000@outlook.com>
2025-06-12 03:11:29 +0300

    Finished Nav&Footer !!

--- frontend/customer-interface/src/components/explore/explore.component.ts
+++ frontend/customer-interface/src/components/explore/explore.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-explore',
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.css'
 })
--- frontend/customer-interface/src/components/navbar/navbar.component.ts
+++ frontend/customer-interface/src/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
 
